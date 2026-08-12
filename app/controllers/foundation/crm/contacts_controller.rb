785c95c31539e46c0078d77ea93900c2efb1c1a0
Vela <velaoc@proton.me>
2026-08-11 18:49:08 -0600

    patch app/controllers/foundation/crm/contacts_controller.rb

--- app/controllers/foundation/crm/contacts_controller.rb
+++ app/controllers/foundation/crm/contacts_controller.rb
@@ -59,7 +59,7 @@ module Foundation
 
       def contact_params
         permitted = params.require(:contact).permit(
-          :first_name, :last_name, :email, :phone, :title, :company_id, :owner_id
+          :first_name, :last_name, :email, :phone, :title, :role, :company_id, :owner_id
         )
         sanitize_org_refs!(permitted, company_id: Company, owner_id: :member)
         permitted
