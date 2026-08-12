103ae7eaef1d85332b587fe8a4fab988f867bd34
Vela <velaoc@proton.me>
2026-08-11 18:42:44 -0600

    add db/migrate/20260813000001_create_crm_properties.rb

---
+++ db/migrate/20260813000001_create_crm_properties.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class CreateCrmProperties < ActiveRecord::Migration[8.0]
+  def change
+    create_table :crm_properties do |t|
+      t.references :organization, null: false, foreign_key: { to_table: :organizations }
+      t.string :address_line_1, null: false, default: ""
+      t.string :address_line_2, default: ""
+      t.string :city, null: false, default: ""
+      t.string :state, default: ""
+      t.string :postal_code, default: ""
+      t.string :country, default: "US"
+      t.integer :price_cents, default: 0, null: false
+      t.decimal :beds, precision: 4, scale: 1, default: 0
+      t.decimal :baths, precision: 4, scale: 1, default: 0
+      t.integer :sqft, default: 0
+      t.string :property_type, default: "single_family", null: false
+      t.string :status, default: "active", null: false
+      t.string :listing_type, default: "sale", null: false
+      t.string :mls_number, default: ""
+      t.text :description
+      t.timestamps
+    end
+
+    add_index :crm_properties, [ :organization_id, :status ]
+  end
+end
